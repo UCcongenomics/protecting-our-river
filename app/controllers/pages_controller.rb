c105fbe51aac8d3e3cf74e835771f5b71b8236fa
wykhuh <wykhuh@users.noreply.github.com>
2019-06-07 23:31:07 -0700

    add static pages and basic content

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,11 @@
+# frozen_string_literal: true
+
 class PagesController < ApplicationController
+  def home; end
+
+  def explore_data; end
+
+  def explore_story; end
+
+  def about_us; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,7 @@
 Rails.application.routes.draw do
+  get 'about_us', to: 'pages#about_us'
+  get 'explore_data', to: 'pages#explore_data'
+  get 'explore_stories', to: 'pages#explore_stories'
+
+  root 'pages#home'
 end
