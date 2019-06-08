b3f0a5e14e5982d5802040a6acff2980e798723e
wykhuh <wykhuh@users.noreply.github.com>
2019-06-07 20:55:54 -0700

    add pages model

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_08_031133) do
+ActiveRecord::Schema.define(version: 2019_06_08_034850) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "pages", force: :cascade do |t|
-    t.text "body", null: false
     t.string "title", null: false
-    t.string "slug", null: false
+    t.text "body", null: false
     t.boolean "draft", default: false, null: false
+    t.string "menu_cd", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["slug"], name: "index_pages_on_slug", unique: true
   end
 
 end
