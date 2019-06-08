c048ed0eaf7a211b92a03411e9b731d3048b4a9f
wykhuh <wykhuh@users.noreply.github.com>
2019-06-07 23:48:50 -0700

    install trestle admin - add admin auth - add tiny mce

---
+++ spec/factories/administrators.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :administrator do
+    email { "MyString" }
+    password_digest { "MyString" }
+    first_name { "MyString" }
+    last_name { "MyString" }
+    remember_token { "MyString" }
+    remember_token_expires_at { "2019-06-07 23:56:32" }
+  end
+end
